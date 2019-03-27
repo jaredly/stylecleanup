1f748d6d0632fe2d551b767f4e3054769b2e1922
Stephen Pittman <stephen.t.pittman@gmail.com>
2019-03-27 08:08:23 -0700

    Add dynamicImport parser plugin

--- analyzeFile.js
+++ analyzeFile.js
@@ -33,7 +33,7 @@ module.exports = (file) => {
   const text = fs.readFileSync(file, 'utf8')
   const ast = parser.parse(text, {
     sourceType: 'module',
-    plugins: ['jsx', 'flow', 'objectRestSpread', 'classProperties'],
+    plugins: ['jsx', 'flow', 'objectRestSpread', 'classProperties', 'dynamicImport'],
   })
   const lines = text.split('\n')
 
